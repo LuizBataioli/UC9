0ddad3fc9c721761de5746c89b016dc75e051c9a
Luiz Bataioli <luizbataioli23@gmail.com>
2024-05-07 21:58:22 -0300

    Atividade Lista de Tarefas

---
+++ ListaDeTarefas/src/main/java/com/mycompany/listadetarefas/ListaDeTarefas.java
@@ -0,0 +1,11 @@
+package com.mycompany.listadetarefas;
+
+public class ListaDeTarefas {
+
+    public static void main(String[] args) {
+        
+        frmListaDeTarefas formListaDeTarefas = new frmListaDeTarefas();
+        formListaDeTarefas.setVisible(true);
+        
+    }
+}
